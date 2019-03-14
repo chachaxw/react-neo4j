e9451f7af7784d8035432d4c7a8c99d55a073b2d
chacha <867571123@qq.com>
2019-03-14 19:16:31 +0800

    Add screen shot and update VisualEditor component

--- README.md
+++ README.md
@@ -42,3 +42,7 @@ You don’t have to ever use `eject`. The curated feature set is suitable for sm
 You can learn more in the [Create React App documentation](https://facebook.github.io/create-react-app/docs/getting-started).
 
 To learn React, check out the [React documentation](https://reactjs.org/).
+
+## Screen Shot
+
+[React Neo4j](!https://github.com/chachaxw/neo4j-react/blob/master/public/screen-shot.png)
--- src/components/VisualEditor.tsx
+++ src/components/VisualEditor.tsx
@@ -26,7 +26,20 @@ class VisualEditor extends Component<any, InternalState> {
                 { id: 2, name: '36楼' },
                 { id: 3, name: 'SEVEN-ELEVEN' },
                 { id: 4, name: '创茶空间' },
-                { id: 5, name: '驿站2号桩' },
+                { id: 5, name: '驿站1号桩' },
+                { id: 6, name: '驿站2号桩' },
+                { id: 7, name: '驿站3号桩' },
+                { id: 8, name: '驿站4号桩' },
+                { id: 9, name: '驿站5号桩' },
+                { id: 10, name: '驿站6号桩' },
+                { id: 11, name: '驿站7号桩' },
+                { id: 12, name: '驿站8号桩' },
+                { id: 13, name: '驿站9号桩' },
+                { id: 14, name: '驿站10号桩' },
+                { id: 15, name: '驿站11号桩' },
+                { id: 16, name: '驿站12号桩' },
+                { id: 17, name: '驿站13号桩' },
+                { id: 18, name: '驿站14号桩' },
             ],
             links: [
                 { value: 1, source: 0, target: 1, relative: 'LINK_TO' },
@@ -35,6 +48,19 @@ class VisualEditor extends Component<any, InternalState> {
                 { value: 3, source: 0, target: 3, relative: 'REFERENCE' },
                 { value: 4, source: 1, target: 4, relative: 'REFERENCE' },
                 { value: 2, source: 0, target: 5, relative: 'REFERENCE' },
+                { value: 2, source: 0, target: 6, relative: 'REFERENCE' },
+                { value: 2, source: 0, target: 7, relative: 'REFERENCE' },
+                { value: 2, source: 0, target: 8, relative: 'REFERENCE' },
+                { value: 2, source: 0, target: 9, relative: 'REFERENCE' },
+                { value: 2, source: 0, target: 10, relative: 'REFERENCE' },
+                { value: 2, source: 0, target: 11, relative: 'REFERENCE' },
+                { value: 2, source: 0, target: 12, relative: 'REFERENCE' },
+                { value: 1, source: 1, target: 13, relative: 'REFERENCE' },
+                { value: 1, source: 1, target: 14, relative: 'REFERENCE' },
+                { value: 1, source: 1, target: 15, relative: 'REFERENCE' },
+                { value: 1, source: 1, target: 16, relative: 'REFERENCE' },
+                { value: 1, source: 1, target: 17, relative: 'REFERENCE' },
+                { value: 1, source: 1, target: 18, relative: 'REFERENCE' },
             ],
         }
     }
@@ -117,8 +143,8 @@ class VisualEditor extends Component<any, InternalState> {
         if (!d3.event.active) {
             this.simulation.alphaTarget(0);
         }
-        d.fx = null;
-        d.fy = null;
+        // d.fx = null;
+        // d.fy = null;
     }
 
     onZoom(svg: any) {
