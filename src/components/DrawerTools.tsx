895442b4c58953807843a746ee636bab84d238e7
chacha <867571123@qq.com>
2019-06-07 23:19:22 +0800

    Update package.json

--- src/components/DrawerTools.tsx
+++ src/components/DrawerTools.tsx
@@ -1,43 +1,24 @@
 import { Drawer, Button } from 'antd';
-import React, { Component, SyntheticEvent } from 'react';
+import React, { FC, Component, SyntheticEvent } from 'react';
 
 interface InternalProps {
   node: any;
-}
-
-interface InternalState {
   visible: boolean;
 }
 
-class DrawerTools extends Component<InternalProps, InternalState> {
+const DrawerTools: FC<InternalProps> = (props) => {
+  const { node, visible, } = props;
 
-  state: InternalState = {
-    visible: false,
-  };
-
-  public showDrawer = () => {
-    this.setState({ visible: true });
-  };
-
-  public onClose = () => {
-    this.setState({ visible: false });
-  };
-
-  public render() {
-    const { node } = this.props;
-
-    return (
-      <Drawer
-        width={240}
-        placement="right"
-        closable={false}
-        title={node.name}
-        onClose={this.onClose}
-        visible={this.state.visible}
-      >
-      </Drawer>
-    );
-  }
+  return (
+    <Drawer
+      width={240}
+      placement="right"
+      closable={false}
+      title={node.name}
+      visible={visible}
+    >
+    </Drawer>
+  );
 }
 
 export default DrawerTools;
